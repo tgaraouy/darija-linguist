63474384f90d5096ae6edd4095bf14ebc8dccf6b
tgaraouy <14682065+tgaraouy@users.noreply.github.com>
2024-08-19 22:26:00 -0700

    Fix font loading and CSP issues

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import './assets/css/main.css'
+import './assets/css/typography.css'
+import './assets/css/layout.css'
+import './assets/css/components.css'
 
 loadFonts()
 
